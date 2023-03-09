fe0b329016e51ed16cba9f19f50765dfe8d9b3c5
Anna Mendrukh <amendrukh@gmail.com>
2023-03-09 21:58:49 +0200

    test

--- code/index.js
+++ code/index.js
@@ -5,6 +5,7 @@ import { req, categorySelect } from "./functions.js";
 
 const EXPORT = document.querySelector("#export");
 const REQ = document.getElementById("req");
+const CLOSE =  document.querySelector("#close");
 
 if (!sessionStorage.isLogin && !document.location.pathname.includes("/authorization")) {
     document.location = "/authorization";
@@ -55,6 +56,9 @@ try{
     REQ.addEventListener("click", () => {
         req("fetch", "https://jsonplaceholder.typicode.com/comments")
     })
+    CLOSE.addEventListener("click", ((e) => {
+        document.location = "/authorization";
+    }))
 }catch(e){
 }
 
